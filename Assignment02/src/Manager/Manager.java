1bab9468757c8140a685064904283365a8d3e062
ThinhPham1999 <thinhphu1234567@gmail.com>
2019-08-01 11:02:23 +0700

    Add files via upload

---
+++ Assignment02/src/Manager/Manager.java
@@ -0,0 +1,82 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Manager;
+
+import Entity.TaxPayer;
+import Tree.AVLBinaryTree;
+import Tree.Node;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author thinh
+ */
+public class Manager {
+
+    public static int menu() {
+        System.out.println("-------TAXPAYER MANAGER-------");
+        System.out.println("1. Load data from file");
+        System.out.println("2. Input & insert data");
+        System.out.println("3. In-order traverse");
+        System.out.println("4. Pre-order traverse");
+        System.out.println("5. Breath-first traverse");
+        System.out.println("6. In-order traverse to file");
+        System.out.println("7. Search by code");
+        System.out.println("8. Delete by code by copying");
+        System.out.println("9. Simple balancing (or AVL tree)");
+        System.out.println("10. Count number of taxpayers");
+        System.out.println("0. Exit");
+        System.out.print("Enter your choice: ");
+        int choice = Validate.checkInputLimit(0, 10);
+        return choice;
+    }
+
+    public static void loadFile(AVLBinaryTree<TaxPayer> list, String filepath) {
+        String thisLine = null;
+        try {
+            FileReader fr = new FileReader("test.txt");
+
+            // open input stream test.txt for reading purpose.
+            BufferedReader br = new BufferedReader(fr);
+
+            while ((thisLine = br.readLine()) != null) {
+                String[] parts = thisLine.split("\\|");
+                list.insertTo(new TaxPayer(parts[0], parts[1], Double.parseDouble(parts[3].trim())));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void visit(Node<TaxPayer> p, FileWriter fw) throws IOException {
+        
+        fw.write(p.getInfo().getCode() + " | " + p.getInfo().getName() + " | "
+                + p.getInfo().getIncome() + " | " + p.getInfo().getDeduct() + " | "
+                + p.getInfo().getTax() + "\n");
+
+        
+    }
+    
+    private static void inOrderFile(Node<TaxPayer> p, FileWriter fw) throws IOException{
+        if (p == null) {
+            return;
+        }
+        inOrderFile(p.left, fw);
+        visit(p, fw);
+        inOrderFile(p.right, fw);
+    }
+    
+    public static void writeFile(AVLBinaryTree<TaxPayer> list) throws IOException{
+        FileWriter fw = new FileWriter("test.txt");
+        inOrderFile(list.root, fw);
+        fw.close();
+    }
+    
+}
---
+++ Assignment02/src/assignment02/Assignment02.java
@@ -0,0 +1,32 @@
+
+package assignment02;
+
+import Entity.TaxPayer;
+import Tree.*;
+import java.io.IOException;
+import Manager.*;
+
+public class Assignment02 {
+
+    
+    public static void main(String[] args) throws IOException {
+        AVLBinaryTree<TaxPayer> list = new AVLBinaryTree<>();
+        
+//        list.insertTo(new TaxPayer("S010", "Nam", 5000));
+//        list.insertTo(new TaxPayer("S020", "Van", 6500));
+//        list.insertTo(new TaxPayer("S030", "Thinh", 7800));
+//        list.insertTo(new TaxPayer("S040", "Nam", 9000));
+//        list.insertTo(new TaxPayer("S050", "Van", 12000));
+//        list.insertTo(new TaxPayer("S025", "Thinh", 3000));
+//        list.insertTo(new TaxPayer("S007", "Thinh", -1));
+//        list.insertTo(new TaxPayer("S008", "Thinh", 01));
+//        list.insertTo(new TaxPayer("S009", "Thinh", 02));
+//        list.delete(new TaxPayer("S010", "", 10));
+        //list.insertTo(new TaxPayer("007", "Thinh", 10000));
+        //System.out.println("S010".compareToIgnoreCase("S011"));
+        //Manager.Manager.writeFile(list);
+        Manager.loadFile(list, "test.txt");
+        list.preOrder(list.root);
+    }
+    
+}
